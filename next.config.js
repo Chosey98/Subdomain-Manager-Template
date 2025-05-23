e17e47dd22147fb60d4fc3bde9e83faca95fad25
Yousef Ashraf <yousefashraftawfiq@gmail.com>
2025-05-23 13:22:18 +0300

    Adjusted config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// Enable reading environment variables at runtime
-	experimental: {
-		serverActions: true,
-	},
 	env: {
 		CLIENT_DATA: process.env.CLIENT_DATA,
 	},
